29059ed73818b5d0b50331a4ec76aecd48e1d7fe
Vasilii Gerasimov <galaxycrab@nebius.com>
2025-12-19 14:12:34 +0200

    Provide Local YDB tree in the debugging panel

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,25 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+// Tree data provider for YDB panel
+class LocalYdbTreeDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+	private _onDidChangeTreeData: vscode.EventEmitter<vscode.TreeItem | undefined | null | void> = new vscode.EventEmitter<vscode.TreeItem | undefined | null | void>();
+	readonly onDidChangeTreeData: vscode.Event<vscode.TreeItem | undefined | null | void> = this._onDidChangeTreeData.event;
+
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
+
+	getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: vscode.TreeItem): Thenable<vscode.TreeItem[]> {
+		// Return empty array for now - you can add items here later
+		return Promise.resolve([]);
+	}
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -9,6 +28,13 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "VibeDB" is now active!');
+
+	// Register YDB tree view
+	const LocalYdbTreeDataProvider = new LocalYdbTreeDataProvider();
+	const ydbTreeView = vscode.window.createTreeView('local-ydb', {
+		treeDataProvider: LocalYdbTreeDataProvider
+	});
+	context.subscriptions.push(ydbTreeView);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
